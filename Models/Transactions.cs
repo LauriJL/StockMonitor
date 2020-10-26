41b8974573cb74395babe3ba7dbb005ee21a427b
LauriJLCareeria <58850493+LauriJLCareeria@users.noreply.github.com>
2020-10-26 21:11:50 +0200

    Transactions/Create
    Dropdown hidden

--- Models/Transactions.cs
+++ Models/Transactions.cs
@@ -11,37 +11,25 @@ namespace StockMonitor_2.Models
 {
     using System;
     using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
-    using System.Globalization;
-
+    
     public partial class Transactions
     {
         public int ID { get; set; }
         public string Kayttaja { get; set; }
         public string OstoMyynti { get; set; }
-        [DataType(DataType.Date)]
-        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:d.M.yyyy}")]
-        public System.DateTime Pvm { get; set; }
+        public Nullable<System.DateTime> Pvm { get; set; }
         public string Yritys { get; set; }
         public int Maara { get; set; }
         public Nullable<int> MaaraForPortfolio { get; set; }
-        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:N}")]
-        //[DataType(DataType.Currency)]
         public decimal aHinta { get; set; }
         public Nullable<decimal> Total { get; set; }
         public Nullable<decimal> TotalForPortfolio { get; set; }
         public string Valuutta { get; set; }
-        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:N}")]
-        //[DataType(DataType.Currency)]
         public Nullable<decimal> Kurssi { get; set; }
         public Nullable<decimal> TotalEuros { get; set; }
-        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:N}")]
-        //[DataType(DataType.Currency)]
         public decimal Kulut { get; set; }
-        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:N}")]
-        //[DataType(DataType.Currency)]
         public Nullable<decimal> Grandtotal { get; set; }
-
+    
         public virtual Currency Currency { get; set; }
         public virtual Users Users { get; set; }
         public virtual TransactionTypes TransactionTypes { get; set; }
